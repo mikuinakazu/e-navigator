46c77e3faa246bacfeca8950c940d8f1df28ac02
稲員未来 <miku.inakazu@gmail.com>
2018-03-30 15:36:01 +0900

    remove cathe

--- app/models/profile.rb
+++
@@ -1,5 +0,0 @@
-class Profile < ApplicationRecord
-  belongs_to :user
-
-  enum sex: { 男性: 0, 女性: 1 }
-end
--- config/routes.rb
+++
@@ -1,5 +0,0 @@
-Rails.application.routes.draw do
-  devise_for :users
-  root 'profiles#index'
-  resources :profiles, only: [:new, :create, :edit, :update]
-end
