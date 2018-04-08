4cfc849b3d7a44cb126bfef60dd23dc40024bed7
稲員未来 <miku.inakazu@gmail.com>
2018-04-08 18:24:56 +0900

    add columns

---
+++ db/migrate/20180408092134_create_interviews.rb
@@ -0,0 +1,10 @@
+class CreateInterviews < ActiveRecord::Migration[5.1]
+  def change
+    create_table :interviews do |t|
+      t.datetime :interview_date
+      t.string   :status
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180313103829) do
+ActiveRecord::Schema.define(version: 20180408092134) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "interviews", force: :cascade do |t|
+    t.datetime "interview_date"
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "profiles", force: :cascade do |t|
     t.string "name"
