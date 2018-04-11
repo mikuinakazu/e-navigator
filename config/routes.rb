c1e9199c1e2a9928af6307db267dabeffb4c1689
稲員未来 <miku.inakazu@gmail.com>
2018-04-11 14:41:22 +0900

    modify routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'profiles#index'
   resources :profiles, only: [:new, :create, :edit, :update]
-  resources :interviews, only: [:index, :new, :create, :edit, :update, :delete]
+  resources :interviews, except: [:show]
 end
