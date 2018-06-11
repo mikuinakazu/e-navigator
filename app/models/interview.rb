b8c2f5fd65f4062d4ccd55cc64a8649ac47a2aca
稲員未来 <miku.inakazu@gmail.com>
2018-06-11 11:11:15 +0900

    make change_date method

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -1,3 +1,11 @@
 class Interview < ApplicationRecord
   belongs_to :user
+
+  def initialize(interview_date)
+    @interview_date = interview_date
+  end
+
+  def change_date
+    return @interview_date.strftime("%Y年%m月%d日 %H時%M分")
+  end
 end
