0c48fa5a39b6a6702ba2a2dd7ab4fbbc9d1f016f
稲員未来 <miku.inakazu@gmail.com>
2018-06-19 23:14:44 +0900

    can save only 1 date per user

--- db/migrate/20180618023708_create_determined_dates.rb
+++ db/migrate/20180618023708_create_determined_dates.rb
@@ -2,7 +2,7 @@ class CreateDeterminedDates < ActiveRecord::Migration[5.1]
   def change
     create_table :determined_dates do |t|
       t.datetime :interview_date
-      t.integer  :user_id
+      t.integer  :user_id, unique: true
       t.timestamps
     end
   end
